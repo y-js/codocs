2954d76f92048bfe32ed461e072ec1cfbb377ba3
Kevin Jahns <kevin.jahns@rwth-aachen.de>
2016-10-30 01:47:44 +0200

    made ready for github.io deployment

--- src/behaviors.js
+++ src/behaviors.js
@@ -1,31 +1,45 @@
 
-
-window.AppBehaviors = {
-  RoomIdentity: {
-    /*
-    We encode a room name
-    */
-    getRoomName: function (roomId) {
-      if (roomId == null) {
-        throw new Error('You must specify room name!')
-      }
-      return btoa(JSON.stringify({
-        type: 'room',
-        roomId: roomId
-      }))
-    },
-    getRoomLink: function (roomId, secret) {
-      return window.location.origin + '/#' + btoa(JSON.stringify({
-        roomId: roomId,
-        secret: secret || null
-      }))
-    },
-    parseRoomLink: function (link) {
-      var p = JSON.parse(atob(link))
-      return {
-        roomName: btoa(JSON.stringify({ type: 'room', roomId: p.roomId })),
-        secret: p.secret
+(function () {
+  var connectorUrl
+  if (window.location.hostname === "codocs.github.io") {
+    connectorUrl = 'https://codocs.herokuapp.com'
+  } else {
+    connectorUrl = 'localhost:1234'
+  }
+  window.AppBehaviors = {
+    RoomIdentity: {
+      properties: {
+        connectorUrl: {
+          type: String,
+          value: connectorUrl
+        }
+      },
+      /*
+      We encode a room name
+      */
+      getRoomName: function (roomId) {
+        if (roomId != null) {
+          return btoa(JSON.stringify({
+            type: 'room',
+            roomId: roomId
+          }))
+        } else {
+          return null
+        }
+      },
+      getRoomLink: function (roomId, secret) {
+        return window.location.origin + '/#' + btoa(JSON.stringify({
+          roomId: roomId,
+          secret: secret || null
+        }))
+      },
+      parseRoomLink: function (link) {
+        var p = JSON.parse(atob(link))
+        return {
+          roomName: btoa(JSON.stringify({ type: 'room', roomId: p.roomId })),
+          secret: p.secret
+        }
       }
     }
   }
-}
+})()
