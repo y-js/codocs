08ecca800b4259119ff51c5d2ce72d0f4a708e3e
Kevin Jahns <kevin.jahns@rwth-aachen.de>
2016-10-31 04:58:55 +0100

    added self-hosted fonts

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -11,7 +11,8 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/fonts/fonts.css'
   ],
   navigateFallback: '/index.html'
 };
