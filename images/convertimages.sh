e31954a45dce6bd3f8b38721112c18294cdc3a74
Kevin Jahns <kevin.jahns@rwth-aachen.de>
2017-01-30 15:14:23 +0100

    added icon in size of 32x32 for use in README.md

--- images/convertimages.sh
+++ images/convertimages.sh
@@ -1,4 +1,5 @@
 convert logo.sketch.svg.png -define icon:auto-resize=64,48,32,16 favicon.ico
+convert logo.sketch.svg.png -resize 32x32   ./manifest/icon-32x32.png
 convert logo.sketch.svg.png -resize 48x48   ./manifest/icon-48x48.png
 convert logo.sketch.svg.png -resize 72x72   ./manifest/icon-72x72.png
 convert logo.sketch.svg.png -resize 96x96   ./manifest/icon-96x96.png
