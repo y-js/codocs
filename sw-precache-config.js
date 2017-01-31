e40592668c026ffb77876bf8af41eb04774c21d0
Kevin Jahns <kevin.jahns@rwth-aachen.de>
2017-01-31 15:15:08 +0100

    improving page load by removing unnecessary dependencies

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -3,27 +3,27 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/images/favicon.ico',
-    '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
-    '/fonts/fonts.css',
     '/yjs-webworker.js',
-    '/bower_components/yjs/y.js',
-    '/bower_components/y-memory/y-memory.js',
-    '/bower_components/y-indexeddb/y-indexeddb.js',
-    '/bower_components/y-websockets-client/y-websockets-client.js',
-    '/bower_components/y-webworker/yjs-webworker-service.js',
-    '/bower_components/ace-builds/src-min-noconflict/mode-markdown.js',
-    '/bower_components/ace-builds/src-min-noconflict/mode-latex.js',
-    '/bower_components/KaTeX/dist/fonts/KaTeX_Main-Regular.woff2',
-    '/bower_components/KaTeX/dist/fonts/KaTeX_Main-Italic.woff2',
-    '/bower_components/pdfjs-dist/build/pdf.worker.min.js'
+    '/manifest.json',
+    '/fonts/fonts.css',
+    'README.md',
+    '/src/global-style.css',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
   ],
   navigateFallback: '/index.html',
   runtimeCaching: [{
     urlPattern: /\/fonts\/(.*)/,
     handler: 'cacheFirst'
   }, {
-    urlPattern: /\/bower_components\/pdftex.js\//,
+    urlPattern: /\/bower_components\//,
     handler: 'cacheFirst'
+  }, {
+    urlPattern: /./,
+    handler: 'fastest',
+    options: {
+      cache: {
+        maxAgeSeconds: 60
+      }
+    }
   }]
 }
